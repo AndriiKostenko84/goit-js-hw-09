f6fa7b2ad583f03f1557450554652f3d221f496d
AndriiKostenko84 <a.kostenko84@gmail.com>
2023-03-23 23:08:24 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
 
+let fancyInterval = null;
+stopBtnEl.disabled = true;
+
+const startBtnListener = startBtnEl.addEventListener("click", onClickStartFancy);
+const stopBtnListener = stopBtnEl.addEventListener("click", onClickStopFancy);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+
+function onClickStartFancy() {
+    startBtnEl.disabled = true;
+    stopBtnEl.disabled = false;
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+
+    fancyInterval = setInterval(() => {
+        bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+
+};
+
+function onClickStopFancy() {
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
+    clearInterval(fancyInterval);
+};
+             
